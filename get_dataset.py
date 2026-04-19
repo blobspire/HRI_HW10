b67ce5497d0d4b5ed613e093b9bf5bd5c4cd360e
Cole Wendrowski <blobspire@gmail.com>
2026-04-19 17:37:48 -0400

    Move forward or back after grabbing

--- get_dataset.py
+++ get_dataset.py
@@ -6,6 +6,7 @@ import time
 from robot import Panda
 from objects import objects
 import pickle
+from tqdm import tqdm
 
 # parameters
 control_dt = 1. / 240.
@@ -37,11 +38,15 @@ panda = Panda(basePosition=[0, 0, 0],
 # main loop
 offset1 = np.array([0.0, 0.0, 0.2])      # reach above the cylinder
 offset2 = np.array([0.0, 0.0, 0.0])      # grab the cylinder
-offset3 = np.array([-0.3, 0.0, 0.1])     # bring it closer
-offset = [offset1, offset2, offset3]
-timesteps = [401, 201, 401]
+offset3 = np.array([-0.3, 0.0, 0.1])     # bring it forward
+offset4 = np.array([0.3, 0.0, 0.1])      # bring it back
+offset = [offset1, offset2, offset3, offset4] # Robot will randomly choose 3 or 4
+# offset = [offset1, offset2, offset3]
+timesteps = [401, 201, 401, 401]
+# timesteps = [401, 201, 401]
 dataset = []
-for idx in range(10):
+number_of_trajectories = 50 # Initial: 10
+for idx in tqdm(range(number_of_trajectories)):
 
     # reset the robot
     panda.reset(jointStartPositions)
@@ -53,6 +58,7 @@ for idx in range(10):
     # perform the expert behavior
     trajectory = []
     for stage in range(3):
+        post_move = np.random.choice([2, 3]) # Decide to move end effector forward or back after picking up the cylinder
         for idx in range(1, timesteps[stage]):
             # close the gripper
             if stage == 2 and idx == 1:
@@ -62,14 +68,14 @@ for idx in range(10):
                     p.stepSimulation()
                     time.sleep(control_dt)
             # move to the goal position
-            panda.move_to_pose(cylinder_position + offset[stage], 
+            panda.move_to_pose(cylinder_position + offset[post_move if stage == 2 else stage], 
                                 ee_quaternion=p.getQuaternionFromEuler([np.pi/2, np.pi/2, np.pi]), positionGain=0.01)
             p.stepSimulation()
             time.sleep(control_dt)
             if idx % 10 == 0:
                 robot_state = panda.get_state()
                 robot_pos = np.array(robot_state["joint-position"])
-                trajectory.append(robot_pos.tolist() + gripper_state + cylinder_position.tolist())
+                trajectory.append(robot_pos.tolist() + gripper_state + cylinder_position.tolist()) # Robot = 11 dims, gripper = 1 dim, cylinder = 3 dims. Total = 15 dims.
     
     # calculate the actions
     trajectory = np.array(trajectory)
@@ -80,5 +86,5 @@ for idx in range(10):
     dataset += trajectory_actions1.tolist() + trajectory_actions2.tolist()
 
 # save the dataset of demonstrations
-pickle.dump(dataset, open("dataset.pkl", "wb"))
+pickle.dump(dataset, open("dataset_bidirectional.pkl", "wb"))
 print("dataset has this many state-action pairs:", len(dataset))
