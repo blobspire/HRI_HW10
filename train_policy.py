8d488e3bbb91a7ed8d471991bead3cba234f9283
Cole Wendrowski <blobspire@gmail.com>
2026-04-19 18:10:51 -0400

    Increase epochs and LR

--- train_policy.py
+++ train_policy.py
@@ -2,6 +2,7 @@ import torch
 import pickle
 from models import Autoencoder
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 
 # import dataset for training
@@ -24,11 +25,11 @@ def train_model(loadname):
 
     # training parameters
     print("[-] training autoencoder")
-    EPOCH = 1000
-    LR = 0.001
+    EPOCH = 10000
+    LR = 0.0001
 
     # initialize model and optimizer
-    model = Autoencoder(state_dim=15, hidden_dim=128, action_dim=9, latent_dim=1)
+    model = Autoencoder(state_dim=15, hidden_dim=256, action_dim=9, latent_dim=1)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
     # initialize dataset
@@ -39,13 +40,13 @@ def train_model(loadname):
     train_set = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
     # main training loop
-    for epoch in range(EPOCH+1):
+    for epoch in tqdm(range(EPOCH+1)):
         for batch, x in enumerate(train_set):
         
             # collect the demonstrated states and actions
             states = x[:, 0:15]
-            actions = x[:, 15:24]
-            actions_hat = model(states, actions)
+            actions = x[:, 15:24] # Actions are 9 dims. 7 joints + 2 pybullet things
+            actions_hat = model(states, actions) # Add bottleneck in middle
 
             # compute the loss between actual and predicted
             loss = model.mse_loss(actions, actions_hat)
@@ -56,9 +57,9 @@ def train_model(loadname):
             optimizer.step()
             
         if epoch % 10 == 0:
-            print(epoch, loss.item())
-            torch.save(model.state_dict(), "model_weights")
+            # print(epoch, loss.item())
+            torch.save(model.state_dict(), "model_weights_bidirectional_simple")
 
 # train models
 if __name__ == "__main__":
-    train_model("dataset.pkl")
+    train_model("dataset_bidirectional_simple.pkl")
